bcd3df0d12fe1b086bfe2a4881246bf84caae207
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2023-02-15 22:08:39 +0200

    feature/lamp-oil: Lamp oil, animation and restart done.

--- src/scripts/player/player.gd
+++ src/scripts/player/player.gd
@@ -3,15 +3,18 @@ extends KinematicBody
 export var speed : float  	= 0
 
 onready var interact_radius = $InteractRadius
+onready var lamp_oil_ui 	= $PlayerUI/VBoxContainer/HBoxContainer/LampOil
 
 # In-game player-specific variables
-var player_lamp_oil : float = 10.0
-var player_health : float 	= 10.0
-var player_inv : Array		= []
+var player_lamp_oil : float = 100.0
+var player_health : int 	= 10
 var player_start : bool 	= true
+var player_game_over : bool = false
+var player_inv : Array		= []
 
-var velocity 			  	= Vector3(0, 0, 0)
+# Player logic defining variables
 var interactables : Array 	= []
+var velocity 			  	= Vector3(0, 0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +22,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if (player_start):
+		$LampOilTimer.autostart = true
 	interactables = interact_radius.interactables
 	walk(5,  "x") if (Input.is_action_pressed("ui_right")) else stop_player()
 	walk(-5, "x") if (Input.is_action_pressed("ui_left"))  else stop_player()
@@ -44,3 +49,11 @@ func walk(vel : float, axis : String):
 func stop_player():
 	velocity.x = lerp(velocity.x, 0, 0.01)
 	velocity.z = lerp(velocity.z, 0, 0.01)
+
+# Decrease the lamp oil value by half second.
+func _on_LampOilTimer_timeout():
+	player_lamp_oil -= 0.5 if player_lamp_oil > 0 else get_tree().reload_current_scene()
+	lamp_oil_ui.value = player_lamp_oil
+
+func _on_LampOil_value_changed(value):
+	print(lamp_oil_ui.value)
